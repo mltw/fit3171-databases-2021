54644ecda0fbc78106bae2cbf54d1891a408505f
mlim0032 <mlim0032@MVAZ2STUL01046.ad.monash.edu>
2021-05-18 12:35:54 +0800

    add ass2b files

---
+++ Assignments/Ass2B/mh-queries.sql
@@ -0,0 +1,84 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--mh-queries.sql
+
+--Student ID:
+--Student Name:
+--Tutorial No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+
+/*
+    Q1
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q2
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q3
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q4
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q5
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q6
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q7
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q8
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q9
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
+
+/*
+    Q10
+*/
+-- PLEASE PLACE REQUIRED SQL STATEMENT FOR THIS PART HERE
+-- ENSURE your query has a semicolon (;) at the end of this answer
+
---
+++ Assignments/Ass2B/mh-triggers.sql
@@ -0,0 +1,49 @@
+--****PLEASE ENTER YOUR DETAILS BELOW****
+--mh-triggers.sql
+
+--Student ID:
+--Student Name:
+--Tutorial No:
+
+/* Comments for your marker:
+
+
+
+
+*/
+
+/* 
+    Q1 
+*/
+/*Please copy your trigger code and any other necessary SQL statements after this line*/
+
+
+
+
+
+
+
+
+
+/* 
+    Q2
+*/
+/*Please copy your trigger code and any other necessary SQL statements after this line*/
+
+
+
+
+
+
+
+
+
+/* 
+    Q3
+*/
+/*Please copy your trigger code and any other necessary SQL statements after this line*/
+
+
+
+
+
